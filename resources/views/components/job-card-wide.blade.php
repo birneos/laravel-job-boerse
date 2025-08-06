2c5d217363219275871c49545a0d5613e6ef37e6
birne <birneos@gmx.net>
2025-08-06 13:13:30 +0200

    create blade component job-card-wide.blade.php

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,16 @@
+  <div  class="p-4 bg-white/5 rounded-xl flex gap-x-6">
+    <div>
+      <img src="https://picsum.photos/seed/{{rand(500,10000000)}}/90/90" class="rounded-xl">
+
+    </div>
+    <div class="flex-1">
+        <a href="">Laracasts</a>
+        <h3>Video Producer</h3>
+        <p>Full Time - From $60,000</p> 
+    </div>
+    <div>
+      <x-tag>Tag</x-tag> 
+      <x-tag>Tag</x-tag> 
+      <x-tag>Tag</x-tag> 
+    </div>
+</div>
