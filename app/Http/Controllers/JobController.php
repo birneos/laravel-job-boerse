f984f916db332113f78e615d0c10c00d4031266d
birne <birneos@gmx.net>
2025-09-14 17:59:37 +0200

    Create Route + JobController

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreJobRequest;
 use App\Http\Requests\UpdateJobRequest;
 use App\Models\Job;
+use App\Models\Tag;
 
 class JobController extends Controller
 {
@@ -13,7 +14,11 @@ class JobController extends Controller
      */
     public function index()
     {
-        //
+        return view('jobs.index', [
+            //'jobs' => Job::with('employer','tags')->latest()->paginate(10),
+            'jobs' => Job::with('employer','tags')->latest()->paginate(),
+            'tags' => Tag::all()
+        ]);
     }
 
     /**
