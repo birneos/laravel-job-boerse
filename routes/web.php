707ce6a04439d78cad7c718d02dac06653943574
birne <birneos@gmx.net>
2025-09-21 16:47:10 +0200

    Route:  Guest or Auth Routes

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[JobController::class,'index']);
 
-Route::get('/register',[RegistredUserController::class,'create']);
-Route::post('/register',[RegistredUserController::class,'store']);
 
-// // Name it LoginController or AuthController or SessionController
-Route::get('/login',[SessionController::class,'create'])->name('login');
-Route::post('/login',[SessionController::class,'store']);
+Route::middleware('guest')->group(function(){
+  
+  Route::get('/register',[RegistredUserController::class,'create']);
+  Route::post('/register',[RegistredUserController::class,'store']);
 
-Route::delete('/logout',[SessionController::class,'destroy']);
+  // // Name it LoginController or AuthController or SessionController
+  Route::get('/login',[SessionController::class,'create'])->name('login');
+  Route::post('/login',[SessionController::class,'store']);
+
+});
+
+
+Route::delete('/logout',[SessionController::class,'destroy'])->middleware('auth');
