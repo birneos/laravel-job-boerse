cb731546c1214527c825699c8881588a77bbc7a2
birne <birneos@gmx.net>
2025-09-21 07:29:14 +0200

    Update RegisteredUserController, create iew

--- app/Http/Controllers/RegistredUserController.php
+++ app/Http/Controllers/RegistredUserController.php
@@ -19,7 +19,7 @@ class RegistredUserController extends Controller
      */
     public function create()
     {
-        //
+        return view('auth.register');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,17 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegistredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[JobController::class,'index']);
 
-Route::get('/register',RegistredUserController::class,'create');
-Route::post('/register',RegistredUserController::class,'store');
+Route::get('/register',[RegistredUserController::class,'create']);
+Route::post('/register',[RegistredUserController::class,'store']);
 
-Route::get('/login',SessionController::class,'create');
-Route::post('/login',SessionController::class,'store');
+// // Name it LoginController or AuthController or SessionController
+Route::get('/login',[SessionController::class,'create'])->name('login');
+Route::post('/login',[SessionController::class,'store']);
 
-Route::delete('/logout',SessionController::class,'destroy');
+Route::delete('/logout',[SessionController::class,'destroy']);
